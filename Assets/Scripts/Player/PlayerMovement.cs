ad39df15bf6fed539000aff11e595a234518974a
xvacorx <theproofthevictors@gmail.com>
2023-11-03 13:12:12 -0300

    Player Movement update

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 2000f;
+    float movementSpeed = 10f;
+
     private Rigidbody2D rb;
-    private void Awake()
+    private float minX, maxX, minY, maxY;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        CalculateScreenBounds();
     }
 
     private void Update()
@@ -17,8 +20,29 @@ public class PlayerMovement : MonoBehaviour
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(horizontalMovement, verticalMovement) * movementSpeed * Time.deltaTime;
+        Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
+        rb.velocity = movement * movementSpeed;
+    }
 
-        rb.velocity = movement;
+    private void CalculateScreenBounds()
+    {
+        Camera mainCamera = Camera.main;
+        Vector3 lowerLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 upperRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        minX = lowerLeft.x;
+        maxX = upperRight.x;
+        minY = lowerLeft.y;
+        maxY = upperRight.y;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+
+        currentPosition.x = Mathf.Clamp(currentPosition.x, minX, maxX);
+        currentPosition.y = Mathf.Clamp(currentPosition.y, minY, maxY);
+
+        transform.position = currentPosition;
     }
 }
